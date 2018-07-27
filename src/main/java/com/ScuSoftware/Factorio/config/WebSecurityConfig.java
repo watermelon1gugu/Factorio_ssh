846eddb467a47513c4b5b73f3d54e796f8068e1e
Arch yi <843874092@qq.com>
2018-07-28 00:21:24 +0800

    增加member

---
+++ src/main/java/com/ScuSoftware/Factorio/config/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.ScuSoftware.Factorio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Configure HttpSecurity as needed (e.g. enable http basic).
+    }
+}
